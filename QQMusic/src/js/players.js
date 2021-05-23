1387c376388302296ab2d75df0c2b1f1a5b3d566
yagamichihaya <935760346@qq.com>
2021-05-23 20:31:33 +0800

    enter

--- QQMusic/src/js/players.js
+++ QQMusic/src/js/players.js
@@ -1,3 +1,4 @@
+
 function Player($audio){
     return new Player.prototype.init($audio)
 }
@@ -7,12 +8,14 @@ Player.prototype = {
     init:function($audio){
         this.$audio = $audio
         this.audio = $audio.get(0)
+        this.audio.volume = 2/3
     },
     currentIndex : -1,
-    playMusic:function(index,music){
+    playMusic:function(index,music){         //播放歌曲
         if(this.currentIndex==index){
             if(this.audio.paused){
                 this.audio.play();
+
             }else {
                 this.audio.pause();
             }
@@ -22,6 +25,31 @@ Player.prototype = {
             console.log(this);
             this.currentIndex = index;
         }
+    },
+    removeMusic:function(index,music){        //删除歌曲
+        
+        
+        this.musicList.splice(index,1)
+    },
+    getAllTime:function(){
+        return this.audio.duration;
+    },
+    getCurrentTime:function(){
+        return this.audio.currentTime;
+    },
+    musicSkip:function(value,isMove){         //跳转时长功能
+        console.log(isMove);
+        if (isMove===true){         //如果小球正在移动，则返回给index.js的变量isMove.同时阻断歌曲当前播放时长的改变，防止移动小球造成音乐断断续续地播放
+            return "True";
+        }
+        else {
+            this.audio.currentTime = this.audio.duration*value
+            
+        }
+        
+    },
+    musicVoiceControl:function(value){
+        this.audio.volume = value
     }
 }
 Player.prototype.init.prototype = Player.prototype
